a2caf216172a8c697a845dcfe4248521542492c6
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-10-16 16:35:57 -0400

    added basic styling for game

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,9 @@ import "./Header.css";
 const Header = props => (
   <div className="header">
     <div className="title">{props.children}</div>
+    <div className="instructions">
+        <p>Click on a leaf to earn points, but don't click on any more than once!</p>
+    </div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
